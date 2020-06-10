7986b6f513935d729c0a5310ef2e657d218baea0
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-10 15:50:54 +0200

    correct method creatListPerson and more als 1 oldest person

--- PavlenkoHW/src/HW20200609_Files_Stream/FilesStream.java
+++ PavlenkoHW/src/HW20200609_Files_Stream/FilesStream.java
@@ -3,8 +3,6 @@ package HW20200609_Files_Stream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,80 +24,57 @@ public class FilesStream {
 
         List<Person> persons = createListPerson(file);
 
-        System.out.println(getPeopleByAge(persons, 40));
-        System.out.println(getOldes(persons));
-        System.out.println(getYoungest(persons));
+        System.out.println("List of persons of a similar age: \n" + getPeopleByAge(persons, 40));
+        System.out.println("Oldest person: " + getOldes(persons));
+        System.out.println("Youngest person: " + getYoungest(persons));
 
     }
 
     public static List<Person> createListPerson(String file) throws IOException {
-        Stream<String> lines = Files.lines(Paths.get(file));
-        List<Person> persons = new ArrayList<>();
         //разделяем информацию по строкам
-        List<String[]> stringList = lines.map(s -> s.split(";"))
-                .collect(Collectors.toList());
-        //create list People
-        for (String[] s : stringList) {
-            persons.add(new Person(s[0], s[1], Integer.parseInt(s[2])));
-        }
-        return persons;
+        Stream<String> lines = Files.lines(Paths.get(file));
+        //create list Person
+        return lines.map(FilesStream::createPerson).collect(Collectors.toList());
+    }
+
+    private static Person createPerson(Object s) {
+        String str = (String) s;
+        //Separate date with regex ";"
+        String[] sArr = str.split(";");
+        return new Person(sArr[0], sArr[1], Integer.parseInt(sArr[2]));
     }
 
     // выдает только 1 персону. А если их несколько? Надо как-то попробовать Лист вернуть
-    public static Person getOldes(List<Person> pList) {
-        return pList.stream().max(Comparator.comparing(Person::getAge)).get();
+    public static List<Person> getOldes(List<Person> pList) {
+        int max = maxAge(pList);
+        return pList.stream().filter(p -> p.getAge() == max).collect(Collectors.toList()); // return all person with max age
+//        return pList.stream().max(Comparator.comparing(Person::getAge)).get(); // - return only 1 person
     }
 
-    public static Person getYoungest(List<Person> pList) {
-        return pList.stream().min(Comparator.comparing(Person::getAge)).get();
+    public static List<Person> getYoungest(List<Person> pList) {
+        int min = minAge(pList);
+        return pList.stream().filter(p -> p.getAge() == min).collect(Collectors.toList()); // return all person with max age
+//        return pList.stream().min(Comparator.comparing(Person::getAge)).get(); //- return only 1 person
     }
 
     public static List<Person> getPeopleByAge(List<Person> pList, int age) {
         return pList.stream().filter(p -> p.getAge() == age).collect(Collectors.toList());
     }
-}
 
-class Person {
-    private String name;
-    private String lastName;
-    private int age;
-
-    public String getName() {
-        return name;
+    public static int maxAge(List<Person> list) {
+        int max = 0;
+        for (Person p : list) {
+            if (p.getAge() > max) max = p.getAge();
+        }
+        return max;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static int minAge(List<Person> list) {
+        int min = 100;
+        for (Person p : list) {
+            if (p.getAge() < min) min = p.getAge();
+        }
+        return min;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Person(String name, String lastName, int age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
--- PavlenkoHW/src/HW20200609_Files_Stream/FilesStreamTest.java
+++ PavlenkoHW/src/HW20200609_Files_Stream/FilesStreamTest.java
@@ -1,4 +1,48 @@
 package HW20200609_Files_Stream;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static HW20200609_Files_Stream.FilesStream.createListPerson;
+import static org.junit.Assert.assertEquals;
+
+/*
+Ivan;Petrov;25
+Piotr;Ivanov;40
+Anna;Petrova;34
+Sergey;Lukichev;40
+ */
 public class FilesStreamTest {
+    Person p1 = new Person("Ivan", "Petrov", 25);
+    Person p2 = new Person("Piotr", "Ivanov", 40);
+    Person p3 = new Person("Anna", "Petrova", 34);
+    Person p4 = new Person("Sergey", "Lukichev", 40);
+
+
+    @Test
+    public void testCreateListPerson() throws IOException {
+
+        File f = new File("people.csv");
+        List<Person> actual = createListPerson(f.getName());
+
+        List<Person> expected = new ArrayList<>();
+        expected.add(p1);
+        expected.add(p2);
+        expected.add(p3);
+        expected.add(p4);
+        assertEquals(expected.size(), actual.size());
+//        for (int i = 0; i < expected.size(); i++) {
+//            assertEquals(expected.get(i), actual.get(i));
+//
+//        }
+        //error. Но они одинаковые...
+//        Expected :HW20200609_Files_Stream.Person<Person{firstName='Ivan', lastName='Petrov', age=25}>
+//        Actual   :HW20200609_Files_Stream.Person<Person{firstName='Ivan', lastName='Petrov', age=25}>
+
+    }
+
 }
